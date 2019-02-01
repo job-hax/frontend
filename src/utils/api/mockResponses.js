e9382854d3a8ae5ad7b9ab2f83c8e6f1a9b9b1e7
Egemen Uslu <egemeny.uslu@gmail.com>
2019-02-01 03:05:05 -0800

    dashborad improvements

--- src/utils/api/mockResponses.js
+++ src/utils/api/mockResponses.js
@@ -7,13 +7,14 @@ export const mockJobApps = {
       "id": 33,
       "applicationStatus": {
         "id": 1,
-        "value": "toapply"
+        "value": "applied"
       },
       "jobTitle": "Senior Android Engineer",
       "company": "Adobe",
-      "companyLogo": "../../src/assets/icons/JobHax-logo-white.svg",
+      "companyLogo": "../../src/assets/icons/JobHax-logo-black.svg",
       "applyDate": "2019-01-24T14:18:12Z",
       "source": "Linkedin.com",
+      "isRejected": false,
     },
     {
       "id": 32,
@@ -23,9 +24,23 @@ export const mockJobApps = {
       },
       "jobTitle": "Android Engineer",
       "company": "Google",
-      "companyLogo": "../../src/assets/icons/JobHax-logo-white.svg",
+      "companyLogo": "../../src/assets/icons/JobHax-logo-black.svg",
       "applyDate": "2019-01-25T14:18:12Z",
       "source": "Hired.com",
+      "isRejected": false,
+    },
+    {
+      "id": 82,
+      "applicationStatus": {
+        "id": 1,
+        "value": "applied"
+      },
+      "jobTitle": "Android Engineer",
+      "company": "OpenPhone ",
+      "companyLogo": "../../src/assets/icons/JobHax-logo-black.svg",
+      "applyDate": "2018-01-23T14:18:12Z",
+      "source": "Hired.com",
+      "isRejected": false,
     },
     {
       "id": 31,
@@ -35,21 +50,23 @@ export const mockJobApps = {
       },
       "jobTitle": "Lead Android Engineer",
       "company": "OpenPhone ",
-      "companyLogo": "../../src/assets/icons/JobHax-logo-white.svg",
+      "companyLogo": "../../src/assets/icons/JobHax-logo-black.svg",
       "applyDate": "2019-01-23T14:18:12Z",
       "source": "Hired.com",
+      "isRejected": true,
     },
     {
       "id": 21,
       "applicationStatus": {
         "id": 1,
-        "value": "interview"
+        "value": "applied"
       },
       "jobTitle": "Android Intern (Summer 2019)",
       "company": "Mozilla",
       "companyLogo": "https://media.licdn.com/dms/image/C4E0BAQGHz8JwrMTQ0A/company-logo_100_100/0?e=2159024400&v=beta&t=klSj-Z-6bTQr-5Tfl4HWe8JwjnboGzei8ncYUMs56Pc",
       "applyDate": "2018-11-14T06:08:19Z",
       "source": "LinkedIn",
+      "isRejected": false,
     },
     {
       "id": 22,
@@ -62,54 +79,59 @@ export const mockJobApps = {
       "companyLogo": "https://media.licdn.com/dms/image/C4D0BAQGi3Ges8PfZmA/company-logo_100_100/0?e=2159024400&v=beta&t=WjWjmAntr33wWOyPgICbdkgBhTRf0-oaFqYq-81KqRI",
       "applyDate": "2018-11-03T02:00:30Z",
       "source": "LinkedIn",
+      "isRejected": false,
     },
     {
       "id": 23,
       "applicationStatus": {
         "id": 1,
-        "value": "rejected"
+        "value": "onsiteinterview"
       },
       "jobTitle": "Android Developer",
       "company": "American Technology Consulting - ATC",
       "companyLogo": "https://media.licdn.com/dms/image/C560BAQHNEpfnEQ8bOQ/company-logo_100_100/0?e=2159024400&v=beta&t=20Ezz636u6LkF1sCtaCCtsfZYYJ2B2W1XWZRiZokOEI",
       "applyDate": "2018-11-03T01:58:09Z",
       "source": "LinkedIn",
+      "isRejected": true,
     },
     {
       "id": 27,
       "applicationStatus": {
         "id": 1,
-        "value": "rejected"
+        "value": "onsiteinterview"
       },
       "jobTitle": "Android Engineer",
       "company": "Earnin ",
-      "companyLogo": "../../src/assets/icons/JobHax-logo-white.svg",
+      "companyLogo": "../../src/assets/icons/JobHax-logo-black.svg",
       "applyDate": "2018-10-22T08:37:45Z",
       "source": "Hired.com",
+      "isRejected": false,
     },
     {
       "id": 28,
       "applicationStatus": {
         "id": 1,
-        "value": "rejected"
+        "value": "onsiteinterview"
       },
       "jobTitle": "Android",
       "company": "Houseparty ",
-      "companyLogo": "../../src/assets/icons/JobHax-logo-white.svg",
+      "companyLogo": "../../src/assets/icons/JobHax-logo-black.svg",
       "applyDate": "2018-10-19T16:17:26Z",
       "source": "Hired.com",
+      "isRejected": false,
     },
     {
       "id": 29,
       "applicationStatus": {
         "id": 1,
-        "value": "rejected"
+        "value": "onsiteinterview"
       },
       "jobTitle": "Android Engineer",
       "company": "Twenty ",
-      "companyLogo": "../../src/assets/icons/JobHax-logo-white.svg",
+      "companyLogo": "../../src/assets/icons/JobHax-logo-black.svg",
       "applyDate": "2018-09-28T10:33:32Z",
       "source": "Hired.com",
+      "isRejected": false,
     },
     {
       "id": 24,
@@ -122,6 +144,7 @@ export const mockJobApps = {
       "companyLogo": "https://media.licdn.com/dms/image/C4D0BAQFlLFOliANaJg/company-logo_100_100/0?e=2159024400&v=beta&t=wsd-p2beESpcXTtj2oi8gn4DGK3jd5JX6MQ0On6VdBU",
       "applyDate": "2018-09-24T09:21:20Z",
       "source": "LinkedIn",
+      "isRejected": false,
     },
     {
       "id": 30,
@@ -131,9 +154,10 @@ export const mockJobApps = {
       },
       "jobTitle": "Android Engineer",
       "company": "Amazon ",
-      "companyLogo": "../../src/assets/icons/JobHax-logo-white.svg",
+      "companyLogo": "../../src/assets/icons/JobHax-logo-black.svg",
       "applyDate": "2018-09-14T14:35:41Z",
       "source": "Hired.com",
+      "isRejected": false,
     },
     {
       "id": 25,
@@ -146,6 +170,7 @@ export const mockJobApps = {
       "companyLogo": "https://media.licdn.com/dms/image/C560BAQHZqDVHQfhJvw/company-logo_100_100/0?e=2159024400&v=beta&t=pMfkCdUmwyyftx0YDOCSNUYGbqsARAXDMyPDwrJ3A0c",
       "applyDate": "2018-08-22T01:15:48Z",
       "source": "LinkedIn",
+      "isRejected": false,
     },
     {
       "id": 26,
@@ -158,6 +183,7 @@ export const mockJobApps = {
       "companyLogo": "https://media.licdn.com/dms/image/C4E0BAQGUZta9AUuRDQ/company-logo_100_100/0?e=2159024400&v=beta&t=QgFzhknQiH66LEWHEIXYSTfhExFv-4Du9TvGeHt-vbo",
       "applyDate": "2018-08-22T01:14:37Z",
       "source": "LinkedIn",
+      "isRejected": true,
     }
   ]
 }
