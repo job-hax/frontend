1ac892b1dbaaa3a8756a3edcd7941e9fbdc1973b
SAKO <sahriyarm@gmail.com>
2019-03-05 23:53:43 -0800

    Revert "dashboard css updates n responsive width"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,42 +7,36 @@ class Header extends Component {
   render() {
     return (
       <div className="header-container">
-
-        <div className="header-left-side-items">
-          <div className="jobhax-logo-container">
-            <Link  to="/">
-              <div className="jobhax-logo"></div>
-            </Link>
-          </div>
-          <div className="search-box" >
-            <img className="header-icon search-icon" src="../../src/assets/icons/SearchIcon@3x.png"></img>
-            <div>
-              <input className="search-input" id="query" onChange={(e) => {e.preventDefault()}}></input>
-            </div>
+        <div className="jobhax-logo-container">
+          <Link  to="/">
+            <div className="jobhax-logo"></div>
+          </Link>
+        </div>
+        <div className="search-box" >
+          <img className="header-icon search-icon" src="../../src/assets/icons/SearchIcon@3x.png"></img>
+          <div  >
+            <input className="search-input" id="query" onChange={(e) => {e.preventDefault()}}></input>
           </div>
         </div>
-
-        <div className="header-right-side-items">
-          <div className="header-icon general">
-            <Link to="/dashboard">
-              <img src="../../src/assets/icons/SyncIcon@3x.png"></img>
-            </Link>
-          </div>
-          <div className="header-icon general">
-            <Link to="/modal">
-              <img src="../../src/assets/icons/StatsIcon@3x.png"></img>
-            </Link>
-          </div>
-          <div className="header-icon general">
-            <Link to="/test1">
-              <img src="../../src/assets/icons/NotifIcon@3x.png"></img>
-            </Link>
-          </div>
-          <div className="header-icon user-icon">
-            <Link to="/test1/">
-              <img src="../../src/assets/icons/SeyfoIcon@3x.png"></img>
-            </Link>
-          </div>
+        <div className="header-icon general">
+          <Link to="/dashboard">
+            <img src="../../src/assets/icons/SyncIcon@3x.png"></img>
+          </Link>
+        </div>
+        <div className="header-icon general">
+          <Link to="/modal">
+            <img src="../../src/assets/icons/StatsIcon@3x.png"></img>
+          </Link>
+        </div>
+        <div className="header-icon general">
+          <Link to="/test1">
+            <img src="../../src/assets/icons/NotifIcon@3x.png"></img>
+          </Link>
+        </div>
+        <div className="header-icon user-icon">
+          <Link to="/test1/">
+            <img src="../../src/assets/icons/SeyfoIcon@3x.png"></img>
+          </Link>
         </div>
       </div>
   );
