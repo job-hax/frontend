b40859c8431fe4d7e14886e5ae4419ae457b48d1
Egemen Uslu <egemeny.uslu@gmail.com>
2019-03-09 21:32:20 -0800

    login page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,47 +7,46 @@ class Header extends Component {
   render() {
     return (
       <div className="header-container">
-        <div className="jobhax-logo-container">
-          <Link to="/">
-            <div className="jobhax-logo"></div>
-          </Link>
-        </div>
-        <div className="search-box">
-          <img className="header-icon search-icon" src="../../src/assets/icons/SearchIcon@3x.png"></img>
-          <div>
-            <input
-              className="search-input"
-              id="query"
-              onChange={e => {
-                e.preventDefault()
-              }}>
-            </input>
+        <div className="left-container">
+          <div className="jobhax-logo-container">
+            <Link to="/">
+              <div className="jobhax-logo" onClick={() => this.props.googleAuth.signOut()}></div>
+            </Link>
+          </div>
+          <div className="search-box">
+            <img className="header-icon search-icon" src="../../src/assets/icons/SearchIcon@3x.png"></img>
+            <div>
+              <input
+                className="search-input"
+                id="query"
+                onChange={e => {
+                  e.preventDefault()
+                }}>
+              </input>
+            </div>
           </div>
         </div>
-        <div className="header-icon general">
-          <Link to="/dashboard">
-            <img src="../../src/assets/icons/SyncIcon@3x.png"></img>
-          </Link>
-        </div>
-        <div className="header-icon general">
-          <Link to="/modal">
-            <img src="../../src/assets/icons/StatsIcon@3x.png"></img>
-          </Link>
-        </div>
-        <div className="header-icon general">
-          <Link to="/test1">
-            <img src="../../src/assets/icons/NotifIcon@3x.png"></img>
-          </Link>
-        </div>
-        <div className="header-icon user-icon">
-          <Link to="/test1/">
-            <img src="../../src/assets/icons/SeyfoIcon@3x.png"></img>
-          </Link>
-        </div>
-        <div className="header-icon user-icon">
-          <span onClick={() => this.props.googleAuth.signOut()}>
-            LOGOUT
-          </span>
+        <div className="right-container">
+          <div className="header-icon general">
+            <Link to="/dashboard">
+              <img src="../../src/assets/icons/SyncIcon@3x.png"></img>
+            </Link>
+          </div>
+          <div className="header-icon general">
+            <Link to="/modal">
+              <img src="../../src/assets/icons/StatsIcon@3x.png"></img>
+            </Link>
+          </div>
+          <div className="header-icon general">
+            <Link to="/test1">
+              <img src="../../src/assets/icons/NotifIcon@3x.png"></img>
+            </Link>
+          </div>
+          <div className="header-icon user-icon">
+            <Link to="/test1/">
+              <img src="../../src/assets/icons/SeyfoIcon@3x.png"></img>
+            </Link>
+          </div>
         </div>
       </div>
     );
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import {Link} from 'react-router-dom';
+import Footer from '../Footer/Footer.jsx';
 
 import './style.scss'
 
@@ -12,15 +14,53 @@ class Login extends Component {
     this.props.googleAuth.signIn();
   }
 
-  render() {
+  handleSignUp(event) {
+    event.preventDefault()
+    console.log(event.target[0].value)
+    console.log(event.target[1].value)
+  }
+
+  generateForm() {
     return (
+      <form onSubmit={this.handleSignUp} className="form-container">
+        <div className="form-element-container">
+          <label>User Name</label>
+          <input className="input-box"></input>
+        </div>
+        <div className="form-element-container">
+          <label>Password</label>
+          <input className="input-box"></input>
+        </div>
+        <button className="social-buttons" >Sign Up </button>
+      </form>
+    )
+  }
+
+  generateLogin() {
+    return(
       <div className="login-container">
-        <div>
-          <button onClick={this.handleLogin}>
-            LOGIN WITH GOOGLE
-          </button>
+        <div className="content-container">
+          <h1>Register</h1>
+          {this.generateForm()}
+        </div>
+        <div className="social-buttons-container">
+          <Link to="/dashboard">
+            <button className="social-buttons" onClick={this.handleLogin}>Sign Up With GOOGLE</button>
+          </Link>
         </div>
       </div>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {this.generateLogin()}
+        <div  className="bottom-fixed-footer">
+          <Footer/>
+        </div> 
+      </div>
+      
     )
   }
 }
