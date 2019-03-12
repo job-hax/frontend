2c06c2aa577e337359aa8ffd9c40b7b6abbdd4d8
Egemen Uslu <egemeny.uslu@gmail.com>
2019-03-11 21:10:36 -0700

    register initial

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {Link} from 'react-router-dom';
 import Footer from '../Footer/Footer.jsx';
+import {registerUserRequest} from '../../utils/api/requests.js';
 
 import './style.scss'
 
@@ -18,20 +19,44 @@ class Login extends Component {
     event.preventDefault()
     console.log(event.target[0].value)
     console.log(event.target[1].value)
+    registerUserRequest.body={
+      first_name: event.target[0].value,
+      last_name: event.target[1].value,
+      username: event.target[2].value,
+      email: event.target[3].value,
+      password: event.target[4].value,
+      password2: event.target[5].value
+    }
   }
 
   generateForm() {
     return (
       <form onSubmit={this.handleSignUp} className="form-container">
         <div className="form-element-container">
+          <label>First Name</label>
+          <input className="input-box"></input>
+        </div>
+        <div className="form-element-container">
+          <label>Last Name</label>
+          <input className="input-box"></input>
+        </div>
+        <div className="form-element-container">
           <label>User Name</label>
+          <input className="input-box"></input>
+        </div>
+        <div className="form-element-container">
+          <label>Email</label>
           <input className="input-box"></input>
         </div>
         <div className="form-element-container">
           <label>Password</label>
           <input className="input-box"></input>
         </div>
-        <button className="social-buttons" >Sign Up </button>
+        <div className="form-element-container">
+          <label>Re-enter Password</label>
+          <input className="input-box"></input>
+        </div>
+        <button className="social-buttons form-button" >Sign Up </button>
       </form>
     )
   }
