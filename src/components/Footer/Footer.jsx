953ac64914eeb90a1a56ba1b92866a8697acdcb8
Sako Mammadov <sako@opengov.com>
2019-03-17 02:49:22 -0700

    update footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,13 +26,13 @@ class Footer extends Component {
                 <div className="footer-content footer-social-links">
                     <div >
                         <a href="https://github.com/job-hax">
-                            <span >GitHub</span>
+                            <span style={{margin: "4px" , color: "white"}}>GitHub</span>
                         </a>
-                        <a href="https://github.com/job-hax">
-                            <span >Slack</span>
+                        <a href="https://jobhax.slack.com">
+                            <span style={{margin: "4px" , color: "white"}}>Slack</span>
                         </a>
-                        <a href="https://github.com/job-hax">
-                            <span >LinkedIn</span>
+                        <a href="https://groups.google.com/forum/#!forum/jobhax">
+                            <span style={{margin: "4px" , color: "white"}}>Forum</span>
                         </a>
                     </div>
                 </div> 
