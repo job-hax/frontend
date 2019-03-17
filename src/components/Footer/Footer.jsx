4a5e0beef39bbea383355690a0eecc908242a791
Yinan Qiu <>
2019-03-17 14:43:55 -0700

    footer design and css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,29 +10,29 @@ class Footer extends Component {
                 <div className="footer-content footer-inside-links">
                     <div>
                         <Link to="/aboutus">
-                            <span style={{margin: "4px" , color: "white"}}>About Us</span>
+                            <span>About Us</span>
                         </Link>
                         <Link to="/underconstruction">
-                            <span style={{margin: "4px" , color: "white"}}>FAQ</span>
+                            <span>FAQ</span>
                         </Link>
                         <Link to="/underconstruction">
-                            <span style={{margin: "4px" , color: "white"}}>Legal</span>
+                            <span>Legal</span>
                         </Link>
                     </div>
                 </div>
                 <div className="footer-content footer-notation">
-                    <span>JobHax 2019<br />All Rights Reserved</span>
+                    <span>JobHax 2019, All Rights Reserved</span>
                 </div>
                 <div className="footer-content footer-social-links">
                     <div >
                         <a href="https://github.com/job-hax">
-                            <span style={{margin: "4px" , color: "white"}}>GitHub</span>
+                            <span>GitHub</span>
                         </a>
                         <a href="https://jobhax.slack.com">
-                            <span style={{margin: "4px" , color: "white"}}>Slack</span>
+                            <span>Slack</span>
                         </a>
                         <a href="https://groups.google.com/forum/#!forum/jobhax">
-                            <span style={{margin: "4px" , color: "white"}}>Forum</span>
+                            <span>Forum</span>
                         </a>
                     </div>
                 </div> 
