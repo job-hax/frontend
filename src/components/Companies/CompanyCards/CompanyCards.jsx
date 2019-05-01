0af84998f425ad7fdcfb1e745b7d87871a96d066
Egemen Uslu <egemeny.uslu@gmail.com>
2019-05-01 05:07:51 -0700

    company reviews pull, filter and display completed

--- src/components/Companies/CompanyCards/CompanyCards.jsx
+++ src/components/Companies/CompanyCards/CompanyCards.jsx
@@ -1,20 +1,74 @@
 import React from "react";
 import classNames from "classnames";
-import faker from "faker";
 
-import Review from "../Reviews/Reviews.jsx";
+import Reviews from "../Reviews/Reviews.jsx";
 import defaultLogo from "../../../assets/icons/JobHax-logo-black.svg";
+import { fetchApi } from "../../../utils/api/fetch_api";
+import {
+  getCompaniesRequest,
+  getReviewsRequest
+} from "../../../utils/api/requests.js";
+import { IS_CONSOLE_LOG_OPEN } from "../../../utils/constants/constants.js";
 
 import "./style.scss";
 import CompanyStats from "../../Partials/CompanyStats/CompanyStats.jsx";
 
-class CompanyReviews extends React.Component {
+class CompanyCards extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isReviewsShowing: false,
-      imageLoadError: true
+      imageLoadError: true,
+      positionsList: [],
+      reviewsList: []
     };
+
+    this.toggleReviewsDisplay = this.toggleReviewsDisplay.bind(this);
+    this.handleSeeReviews = this.handleSeeReviews.bind(this);
+  }
+
+  toggleReviewsDisplay() {
+    this.setState({
+      isReviewsShowing: !this.state.isReviewsShowing
+    });
+  }
+
+  handleSeeReviews() {
+    let newPositionUrl =
+      getCompaniesRequest.url + this.props.company.id + "/positions";
+    getCompaniesRequest.config.headers.Authorization = this.props.token;
+    fetchApi(newPositionUrl, getCompaniesRequest.config).then(response => {
+      if (response.ok) {
+        this.setState({
+          positionsList: response.json.data
+        });
+        IS_CONSOLE_LOG_OPEN &&
+          console.log(
+            "company positions response json data",
+            response.json.data
+          );
+      }
+    });
+    let newReviewsUrl =
+      getReviewsRequest.url +
+      "?company_id=" +
+      this.props.company.id +
+      "&all_reviews=true";
+    getReviewsRequest.config.headers.Authorization = this.props.token;
+    fetchApi(newReviewsUrl, getReviewsRequest.config).then(response => {
+      if (response.ok) {
+        this.setState({
+          reviewsList: response.json.data
+        });
+        IS_CONSOLE_LOG_OPEN &&
+          console.log(
+            "company get all reviews response json data",
+            response.json.data
+          );
+      }
+    });
+
+    this.toggleReviewsDisplay();
   }
 
   render() {
@@ -38,29 +92,40 @@ class CompanyReviews extends React.Component {
               <div className="company-name">{company.company}</div>
             </div>
             <div className="company-card-info" />
-            {this.props.company.review_count != 0 && (
-              <div
-                className="see-reviews-button"
-                onClick={() =>
-                  this.setState({
-                    isReviewsShowing: !this.state.isReviewsShowing
-                  })
-                }
-              >
-                See {this.props.company.review_count} reviews
-              </div>
-            )}
+            {this.props.company.review_count != 0 &&
+              (!this.state.isReviewsShowing ? (
+                <div
+                  className="see-reviews-button"
+                  onClick={this.handleSeeReviews}
+                >
+                  See {this.props.company.review_count} reviews
+                </div>
+              ) : (
+                <div
+                  className="see-reviews-button"
+                  onClick={this.toggleReviewsDisplay}
+                >
+                  See less...
+                </div>
+              ))}
           </div>
           <div className="company-card-right">
             <CompanyStats company={this.props.company} />
           </div>
         </div>
-        <div className={reviewsClass}>
-          <Review />
-        </div>
+        {this.state.reviewsList.length != 0 && (
+          <div className={reviewsClass}>
+            <Reviews
+              reviewsList={this.state.reviewsList}
+              positionsList={this.state.positionsList}
+              company_id={this.props.company.id}
+              token={this.props.token}
+            />
+          </div>
+        )}
       </div>
     );
   }
 }
 
-export default CompanyReviews;
+export default CompanyCards;
