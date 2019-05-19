4183d1ff96a60db392e5cfb318c7e21bed582374
Egemen Uslu <egemeny.uslu@gmail.com>
2019-05-19 12:22:29 -0700

    config.js cache

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-export const IS_MOCKING = false;
-
-export const googleClientId = "!!! Put your Google Client ID here";
-
-export const jobHaxClientId = "!!! Put your JobHax Client ID here";
-
-export const jobHaxClientSecret = "!!! Put your JobHax Client Secret here";
-
-export const reCaptchaV3SiteKey = "!!! Put your Recaptcha Site Key here";
