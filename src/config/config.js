3b98a9ceceb6b8a6b465a7e4efb760e46f4b515f
EgemenUslu <44197067+EgemenUslu@users.noreply.github.com>
2019-05-19 12:30:10 -0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,7 @@
 export const IS_MOCKING = false;
 
+export const IS_RECAPTCHA_ENABLED = true;
+
 export const googleClientId = "!!! Put your Google Client ID here";
 
 export const jobHaxClientId = "!!! Put your JobHax Client ID here";
