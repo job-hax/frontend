12ff8e871a9ff19276acfc807fa0a0910d5d758e
sakows <sahriyarm@gmail.com>
2019-05-28 14:35:56 +0000

    add config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,10 @@ export const IS_MOCKING = false;
 
 export const IS_RECAPTCHA_ENABLED = true;
 
-export const googleClientId = "!!! Put your Google Client ID here";
+export const googleClientId = "204318009467-8qnvhkhiaa8v71vp3g3cvr4895tpc2ft.apps.googleusercontent.com";
 
-export const jobHaxClientId = "!!! Put your JobHax Client ID here";
+export const jobHaxClientId = 'WET9imCJdPEHPehSsITMSLcJTA8zIb8Pyo0N6Ttv';
 
-export const jobHaxClientSecret = "!!! Put your JobHax Client Secret here";
+export const jobHaxClientSecret = '6lIFHZsjUgeBINM0KNszSIb7K6Yqe4nqYZpC9UnMOW6wHK7PV6gkVmwYW1pVfQRkDsgNY4jnV5QtLnjGPYVCkjIbzERIsXmiIF5ly9tGnrIbtq6mzcZCovoSKpf31wq0';
 
-export const reCaptchaV3SiteKey = "!!! Put your Recaptcha Site Key here";
+export const reCaptchaV3SiteKey = "6LfOH6IUAAAAAL4Ezv-g8eUzkkERCWlnnPq_SdkY";
