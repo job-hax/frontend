26c5a934462d217fe2aafc3e1ed1005acf3e2832
sakows <sahriyarm@gmail.com>
2019-08-10 00:59:01 +0000

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,11 @@
 export const IS_MOCKING = false;
-
 export const IS_RECAPTCHA_ENABLED = true;
+export const googleClientId = "172109409716-k5e0vv57s061nopjip7q61q22bbasehd.apps.googleusercontent.com";
+//JobHax beta export const googleClientId = "204318009467-8qnvhkhiaa8v71vp3g3cvr4895tpc2ft.apps.googleusercontent.com";
 
-export const googleClientId = "204318009467-8qnvhkhiaa8v71vp3g3cvr4895tpc2ft.apps.googleusercontent.com";
-
-export const jobHaxClientId = 'WET9imCJdPEHPehSsITMSLcJTA8zIb8Pyo0N6Ttv';
-
-export const jobHaxClientSecret = '6lIFHZsjUgeBINM0KNszSIb7K6Yqe4nqYZpC9UnMOW6wHK7PV6gkVmwYW1pVfQRkDsgNY4jnV5QtLnjGPYVCkjIbzERIsXmiIF5ly9tGnrIbtq6mzcZCovoSKpf31wq0';
-
+export const jobHaxClientId = 'c6q6Ympb76pN4KA7NhH3MIqlKjlX6QnRy6OYJ30T';
+//Old one: WET9imCJdPEHPehSsITMSLcJTA8zIb8Pyo0N6Ttv  
+export const jobHaxClientSecret = 'PVvBbRTjDE1R56NUMQRQ12mnPBqFVfr1TjDbyEtlJzGMJth7bo2oGOQKlzMxMRmcEf3blJMTSgD2pq5I1SSRpzamSiC3w0rH2asCIzSgMHRIUrPekm6v3uhAOayHDkmt';
+//Old one: 6lIFHZsjUgeBINM0KNszSIb7K6Yqe4nqYZpC9UnMOW6wHK7PV6gkVmwYW1pVfQRkDsgNY4jnV5QtLnjGPYVCkjIbzERIsXmiIF5ly9tGnrIbtq6mzcZCovoSKpf31wq0
 export const reCaptchaV3SiteKey = "6LfOH6IUAAAAAL4Ezv-g8eUzkkERCWlnnPq_SdkY";
-
 export const googleApiKey = "AIzaSyBnF8loY6Vqhs4QWTM_fWCP93Xidbh1kYo";
